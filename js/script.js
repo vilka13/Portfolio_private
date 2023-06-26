699c3e2154f72b1aee4664efe3022f32b1ce0df9
vilka13 <lukumer1997@gmail.com>
2023-06-26 15:53:38 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,60 +1,9 @@
-function validateForm() {
-    var blackFont = "#000000";
-    var redFont = "#FF0000";
 
-    var myModal = new bootstrap.Modal(document.getElementById("exampleModal"), {
-        keyboard: false
-    });
-
-    var name = document.getElementById("name").value;
-    if (name == "") {
-        document.getElementById("labelName").innerText = "Imie nie może być puste";
-        document.getElementById("labelName").style.color = redFont;
-        return false;
-    } else {
-        document.getElementById("labelName").innerText = "Wpisz imię";
-        document.getElementById("labelName").style.color = blackFont;
-    }
-
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    var email = document.getElementById("email").value;
-    if (email == "") {
-        document.getElementById("labelEmail").innerHTML = "E-mail nie może być pusty";
-        document.getElementById("labelEmail").style.color = redFont;
-        return false;
-    } else if (!re.test(email)) {
-        document.getElementById("labelEmail").innerHTML = "Nieprawidłowy format wiadomości e-mail";
-        document.getElementById("labelEmail").style.color = redFont;
-        return false;
-    } else {
-        document.getElementById("labelEmail").innerText = "Wpisz Email";
-        document.getElementById("labelEmail").style.color = blackFont;
-    }
-
-    var subject = document.getElementById("subject").value;
-    if (subject == "") {
-        document.getElementById("labelSubject").innerHTML = "Temat nie może być pusty";
-        document.getElementById("labelSubject").style.color = redFont;
-        return false;
-    } else {
-        document.getElementById("labelSubject").innerText = "Wpisz temat";
-        document.getElementById("labelSubject").style.color = blackFont;
-    }
-
-    var message = document.getElementById("message").value;
-    if (message == "") {
-        document.getElementById("labelMessage").innerHTML = "Wiadomość nie może być pusta";
-        document.getElementById("labelMessage").style.color = redFont;
-        return false;
-    } else {
-        document.getElementById("labelMessage").innerText = "Wpisz wiadomość";
-        document.getElementById("labelMessage").style.color = blackFont;
-    }
-
-    myModal.toggle()
-};
 
 function calculate() {
+    let totalSum = 0;
+
+    function calculate() {
     let totalSum = 0;
 
     // C#
