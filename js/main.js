8688256457dba3ecd2d95a71b78a845a38ed0a30
vilka13 <lukumer1997@gmail.com>
2023-06-28 08:54:06 +0200

    ipdate

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").match
 if (localStorage.getItem('darkMode') === 'dark') {
     btnDarkMode.classList.add("dark-mode-btn--active");
     document.body.classList.add("dark");
-
+    
 } else if (localStorage.getItem("darkMode") === "light") {
     btnDarkMode.classList.remove("dark-mode-btn--active");
     document.body.classList.remove("dark");
@@ -48,90 +48,90 @@ btnDarkMode.onclick = function () {
 
 
 
-// main anim
+// main anim 
 
 function fitElementToParent(el, padding) {
-    var timeout = null;
-    function resize() {
-        if (timeout) clearTimeout(timeout);
-        anime.set(el, {scale: 1});
-        var pad = padding || 0;
-        var parentEl = el.parentNode;
-        var elOffsetWidth = el.offsetWidth - pad;
-        var parentOffsetWidth = parentEl.offsetWidth;
-        var ratio = parentOffsetWidth / elOffsetWidth;
-        timeout = setTimeout(anime.set(el, {scale: ratio}), 10);
-    }
-    resize();
-    window.addEventListener('resize', resize);
+var timeout = null;
+function resize() {
+    if (timeout) clearTimeout(timeout);
+    anime.set(el, {scale: 1});
+    var pad = padding || 0;
+    var parentEl = el.parentNode;
+    var elOffsetWidth = el.offsetWidth - pad;
+    var parentOffsetWidth = parentEl.offsetWidth;
+    var ratio = parentOffsetWidth / elOffsetWidth;
+    timeout = setTimeout(anime.set(el, {scale: ratio}), 10);
+}
+resize();
+window.addEventListener('resize', resize);
 }
 
 var sphereAnimation = (function() {
 
-    var sphereEl = document.querySelector('.sphere-animation');
-    var spherePathEls = sphereEl.querySelectorAll('.sphere path');
-    var pathLength = spherePathEls.length;
-    var hasStarted = false;
-    var aimations = [];
+var sphereEl = document.querySelector('.sphere-animation');
+var spherePathEls = sphereEl.querySelectorAll('.sphere path');
+var pathLength = spherePathEls.length;
+var hasStarted = false;
+var aimations = [];
 
-    fitElementToParent(sphereEl);
+fitElementToParent(sphereEl);
 
-    var breathAnimation = anime({
-        begin: function() {
-            for (var i = 0; i < pathLength; i++) {
-                aimations.push(anime({
-                    targets: spherePathEls[i],
-                    stroke: {value: ['rgb(36,175,36)', 'rgba(80,80,80,.35)'], duration: 500},
-                    translateX: [2, -4],
-                    translateY: [2, -4],
-                    easing: 'easeOutQuad',
-                    autoplay: false
-                }));
-            }
-        },
-        update: function(ins) {
-            aimations.forEach(function(animation, i) {
-                var percent = (1 - Math.sin((i * .35) + (.0022 * ins.currentTime))) / 2;
-                animation.seek(animation.duration * percent);
-            });
-        },
-        duration: Infinity,
+var breathAnimation = anime({
+    begin: function() {
+    for (var i = 0; i < pathLength; i++) {
+        aimations.push(anime({
+        targets: spherePathEls[i],
+        stroke: {value: ['rgb(36,175,36)', 'rgba(80,80,80,.35)'], duration: 500},
+        translateX: [2, -4],
+        translateY: [2, -4],
+        easing: 'easeOutQuad',
         autoplay: false
+        }));
+    }
+    },
+    update: function(ins) {
+    aimations.forEach(function(animation, i) {
+        var percent = (1 - Math.sin((i * .35) + (.0022 * ins.currentTime))) / 2;
+        animation.seek(animation.duration * percent);
     });
+    },
+    duration: Infinity,
+    autoplay: false
+});
 
-    var introAnimation = anime.timeline({
-        autoplay: false
-    })
-        .add({
-            targets: spherePathEls,
-            strokeDashoffset: {
-                value: [anime.setDashoffset, 0],
-                duration: 3900,
-                easing: 'easeInOutCirc',
-                delay: anime.stagger(190, {direction: 'reverse'})
-            },
-            duration: 2000,
-            delay: anime.stagger(60, {direction: 'reverse'}),
-            easing: 'linear'
-        }, 0);
+var introAnimation = anime.timeline({
+    autoplay: false
+})
+.add({
+    targets: spherePathEls,
+    strokeDashoffset: {
+    value: [anime.setDashoffset, 0],
+    duration: 3900,
+    easing: 'easeInOutCirc',
+    delay: anime.stagger(190, {direction: 'reverse'})
+    },
+    duration: 2000,
+    delay: anime.stagger(60, {direction: 'reverse'}),
+    easing: 'linear'
+}, 0);
 
-    var shadowAnimation = anime({
-        targets: '#sphereGradient',
-        x1: '25%',
-        x2: '25%',
-        y1: '0%',
-        y2: '75%',
-        duration: 30000,
-        easing: 'easeOutQuint',
-        autoplay: false
+var shadowAnimation = anime({
+    targets: '#sphereGradient',
+    x1: '25%',
+    x2: '25%',
+    y1: '0%',
+    y2: '75%',
+    duration: 30000,
+    easing: 'easeOutQuint',
+    autoplay: false
     }, 0);
 
-    function init() {
-        introAnimation.play();
-        breathAnimation.play();
-        shadowAnimation.play();
-    }
+function init() {
+    introAnimation.play();
+    breathAnimation.play();
+    shadowAnimation.play();
+}
 
-    init();
+init();
 
 })();
