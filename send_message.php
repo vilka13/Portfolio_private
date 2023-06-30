d56c0bfeaad50da7152fc7d9c49966492bbbb57d
vilka13 <lukumer1997@gmail.com>
2023-06-30 12:19:57 +0200

    update

--- send_message.php
+++ send_message.php
@@ -5,29 +5,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    // Адрес электронной почты, на который нужно отправить сообщение
-    $to = "123@mail.com";
-
-    // Заголовок письма
+    $to = "stanislauolshevski@sofreelancer.pl";
     $subject = "New Message from Website";
-
-    // Текст письма
     $email_message = "Name: $name\n";
     $email_message .= "Company: $company\n";
     $email_message .= "Email: $email\n";
     $email_message .= "Message: $message\n";
-
-    // Дополнительные заголовки
     $headers = "From: $email";
 
-    // Отправка письма
     if (mail($to, $subject, $email_message, $headers)) {
         // Сообщение успешно отправлено
-        header("Location: contacts.html");
-        exit();
+        echo "success";
     } else {
         // Произошла ошибка при отправке сообщения
-        echo "Sorry, something went wrong.";
+        echo "error";
     }
+    exit(); // Добавлено для прерывания выполнения скрипта после отправки сообщения
 }
 ?>
